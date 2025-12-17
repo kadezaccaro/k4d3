9f9046f8217ab7debb632afa4139016345ace6e7
kade <58316992+kadezaccaro@users.noreply.github.com>
2025-12-16 21:07:56 -0500

    Improve navbar with centered links and active styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,27 +2,25 @@ import { NavLink, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <header className="centering-header">
-      <nav id="navbar">
-        <Link to="/">
-          <h1 id="logo">K4D3&trade;</h1>
-        </Link>
+    <nav id="navbar">
+      <Link to="/">
+        <h1 id="logo">K4D3&trade;</h1>
+      </Link>
 
-        <div id="nav-links">
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/">Work</NavLink>
-            </li>
-            <li>
-              <NavLink to="/contact">Contact</NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+      <div id="nav-links">
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
