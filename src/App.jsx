bca7d0ecc2d81c0dd18f94db3a85b264734e3102
kade <58316992+kadezaccaro@users.noreply.github.com>
2025-12-18 18:17:29 -0500

    Add page transitions with Motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,58 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { AnimatePresence } from "framer-motion";
+import PageTransition from "./components/PageTransition";
+import { useEffect } from "react";
+import EmailCTA from "./components/EmailCTA";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
+
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      <Routes location={location} key={location.pathname}>
+        <Route
+          path="/"
+          element={
+            <PageTransition>
+              <Home />
+            </PageTransition>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <PageTransition>
+              <Contact />
+            </PageTransition>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
-    <>
-      <Router>
-        <NavBar />
-
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <NavBar />
+      <AnimatedRoutes />
+      <EmailCTA />
+      <Footer />
+    </Router>
   );
 }
 
